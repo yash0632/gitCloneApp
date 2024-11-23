0418b2f45aadc97d3c0d675ccd0eb3ec26fd49d5
yash0632 <yashsharma0632@gmail.com>
2024-11-23 23:49:08 +0530

    git hash-object

--- app/main.js
+++ app/main.js
@@ -83,7 +83,8 @@ async function getHashObject(){
     
 
     var compressedData = zlib.deflateSync(gitData).toString();
-    const fullPath = path.resolve("test.txt");
+    const fullPath = path.resolve("main.js");
+    console.log(fullPath);
     const pathDir = path.dirname(fullPath);
     console.log(pathDir);
     fs.writeFileSync(path.join(pathDir,".git","objects",hashedGitFileName.substring(0,2),hashedGitFileName.substring(2,40)),compressedData);
