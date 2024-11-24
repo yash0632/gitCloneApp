75c88a70f0712566255bc2a7d7eb2b247162934c
yash0632 <yashsharma0632@gmail.com>
2024-11-24 16:01:41 +0530

    hashobject

--- app/main.js
+++ app/main.js
@@ -93,7 +93,7 @@ async function getHashObject(){
         console.error('An error occurred:', err);
         process.exitCode = 1;
       }
-      compressedData = buffer.toString()
+      compressedData = buffer.toString('base64')
     })
 
     
