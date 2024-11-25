348c43901267eded588b584c9de685ff1cf77be1
yash0632 <yashsharma0632@gmail.com>
2024-11-26 02:01:22 +0530

    write-tree

--- app/main.js
+++ app/main.js
@@ -190,9 +190,9 @@ function dirTreeSha(directory){
       let newDirectory = path.join(directory,directoryFiles[i]);
       let dirHash = dirTreeSha(newDirectory);
       treeContent = treeContent + `40000 ${directoryFiles[i]}\0${dirHash[1]}`
-      if(directoryFiles[i] != 'node_modules'){
-        size += dirHash[2];
-      }
+      
+      size += dirHash[2];
+    
       
     }
   }
